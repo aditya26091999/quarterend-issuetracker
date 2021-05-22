ad7fd9f7f7a26ecd78a1c4399274649313502c49
Aditya Kawale <adityamkawale@gmail.com>
2021-05-22 23:24:13 +0530

    #15 added admin-register/admin-login functionality

--- SourceCode/DjangoClient/README.md
+++
@@ -1,2 +0,0 @@
-This will have client-webapp [Actual Web-app for clients]<br>
-People can track their issues, manage data here in this app.
---
+++ SourceCode/DockerApp/API/dbQueries.py
@@ -0,0 +1,28 @@
+companyRegisteration_sql = "INSERT INTO globalcompany_schema.company (company_id, company_name,company_address,company_admin_email,company_admin_password) VALUES (%s, %s, %s, %s, %s)"
+companySpecificSchemaCreation_sql = "CREATE SCHEMA "
+userTableCreation_sql = "CREATE TABLE {}.user (  user_id VARCHAR(64) NOT NULL,  user_name VARCHAR(255) NOT NULL,  user_email VARCHAR(255) NOT NULL,  user_password VARCHAR(30) NOT NULL,  PRIMARY KEY (user_id),  UNIQUE INDEX user_email_UNIQUE (user_email ASC) VISIBLE)"
+issueTrackerTableCreation_sql = """CREATE TABLE {}.issuetracker (
+  issue_id VARCHAR(64) NOT NULL,
+  issue_title VARCHAR(255) NOT NULL,
+  issue_description VARCHAR(255) NOT NULL,
+  issue_submittedby VARCHAR(64) NOT NULL,
+  issue_assignedto VARCHAR(64) NULL,
+  issue_date DATE NOT NULL,
+  issue_priority INT NOT NULL DEFAULT 3,
+  issue_resolved TINYINT NOT NULL DEFAULT 0,
+  issue_solution VARCHAR(255) NULL,
+  PRIMARY KEY (issue_id),
+  INDEX fk_submittedby_idx (issue_submittedby ASC) VISIBLE,
+  INDEX fk_assignedto_idx (issue_assignedto ASC) VISIBLE,
+  CONSTRAINT fk_submittedby
+    FOREIGN KEY (issue_submittedby)
+    REFERENCES {}.user (user_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_assignedto
+    FOREIGN KEY (issue_assignedto)
+    REFERENCES {}.user (user_id)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE);"""
+
+adminLogin_sql = "SELECT company.company_id,company.company_name,company.company_address, company.company_admin_email FROM globalcompany_schema.company WHERE company.company_admin_email=%s AND company.company_admin_password=%s"
